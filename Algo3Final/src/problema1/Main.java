98d3fefce1f9d3b64cc2bc32ca66c3b416b3d458
martincammi@gmail.com <martincammi@gmail.com@6a4e8b92-d0e5-a920-00eb-13bb9fe8d438>
2011-05-14 16:03:03 +0000

    Se cambiaron de paquete los problemas

---
+++ Algo3Final/src/problema1/Main.java
@@ -0,0 +1,204 @@
+package problema1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String res; 
+		System.out.print("Test1: ");
+		Converter converter1 = new Converter("5 3 1 3 4");
+		res = resolverProblemaMochila(converter1);
+		System.out.println( (res.equals("1 4 sum:5")) ? "OK" : "Fail: " + res);  
+				
+		System.out.print("Test2: ");				
+		Converter converter2 = new Converter("10 4 9 8 4 2");
+		res = resolverProblemaMochila(converter2);
+		System.out.println( (res.equals("8 2 sum:10")) ? "OK" : "Fail: " + res);
+		
+		System.out.print("Test3: ");
+		Converter converter3 = new Converter("20 4 10 5 7 4");
+		res = resolverProblemaMochila(converter3);
+		System.out.println( (res.equals("10 5 4 sum:19")) ? "OK" : "Fail: " + res);
+
+		System.out.print("Test4: ");
+		Converter converter4 = new Converter("90 8 10 23 1 2 3 4 5 7");
+		res = resolverProblemaMochila(converter4);
+		System.out.println( (res.equals("10 23 1 2 3 4 5 7 sum:55")) ? "OK" : "Fail: " + res);
+		
+		System.out.print("Test5: ");		
+		Converter converter5 = new Converter("45 8 4 10 44 43 12 9 8 2");
+		res = resolverProblemaMochila(converter5);
+		System.out.println( (res.equals("4 10 12 9  2 sum:45")) ? "OK" : "Fail: " + res);
+		
+		//Extras
+		System.out.print("Test6: ");
+		Converter converter6 = new Converter("35 6 34 33 10 12 11 2"); //Deberia dar 10 12 11 2
+		res = resolverProblemaMochila(converter6);
+		System.out.println( (res.equals("10 12 11 2 sum:35")) ? "OK" : "Fail: " + res);
+		
+		System.out.print("Test7: ");
+		Converter converter7 = new Converter("6 5 1 2 3 4 5"); //Deberia dar 1,2,3
+		res = resolverProblemaMochila(converter7);
+		System.out.println( (res.equals("1 2 3 sum:6")) ? "OK" : "Fail: " + res);
+		
+		System.out.print("Test8: ");
+		Converter converter8 = new Converter("6 5 5 4 1 2 3"); //Deberia dar 1,2,3
+		res = resolverProblemaMochila(converter8);
+		System.out.println( (res.equals("1 2 3 sum:6")) ? "OK" : "Fail: " + res);
+		
+		System.out.print("Test9: ");
+		Converter converter9 = new Converter("5 3 5 3 2"); //Deberia dar 3,2
+		System.out.println( (res.equals("3 2 sum:6")) ? "OK" : "Fail: " + res);
+	}
+
+	public static String resolverProblemaMochila(Converter converter){
+		int W = converter.maximo; //Maximo tamaño de cinta "w" 
+		int numCanciones = converter.numCanciones; //Cantidad de canciones "n" //Replace by pistas
+		int [] canciones = converter.canciones; //Vector de costo/beneficio
+		int [] cost = canciones;
+		int [] ben = canciones;
+		int usoMax [][] = converter.getUsoMax();
+		int rastroMigajas [][] = converter.getRastroMigajas();
+		
+		List solucion = new ArrayList();
+		
+		for (int w = 1; w <= W; ++w){
+			for (int i = 1; i < numCanciones+1; ++i){
+				if(cost[i-1] <= w){
+					if(usoMax[i-1][w-cost[i-1]] + cost[i-1] > usoMax[i-1][w]){
+						usoMax[i][w] = usoMax[i-1][w-cost[i-1]] + cost[i-1];
+						rastroMigajas[i][w] = 1;
+					}else if (usoMax[i-1][w-cost[i-1]] + cost[i-1] < usoMax[i-1][w]) {
+						usoMax[i][w] = usoMax[i-1][w];
+						rastroMigajas[i][w] = 1;
+					}else{
+						//if(rastroMigajas[i-1][w-cost[i-1]] = 0)
+							usoMax[i][w] = usoMax[i-1][w];
+						rastroMigajas[i][w] = 1;
+					}
+				}else{
+					usoMax[i][w] = usoMax[i-1][w];
+				}
+			}
+		}
+
+		//converter.mostrarMatriz();
+		System.out.println("");
+		//converter.mostrarMatrizMigajas();
+		converter.mostrarSuma();
+
+		int k = W;
+		for (int i = numCanciones; i > 0 ; i--) {
+			if(rastroMigajas[i][k] == 1){
+				solucion.add(canciones[i-1]); 
+				k = k - cost[i-1];
+			}
+		}
+		String solucionStr = "";
+		for (int i = 0; i < solucion.size()-1 ; i++) {
+			solucionStr = solucion.get(i) + solucionStr + " ";
+		}
+		solucionStr = solucionStr + "sum:" + converter.getSuma();
+		System.out.println(solucionStr);
+		return solucionStr;
+	}
+	
+	private static class Converter{
+		
+		int maximo;
+		int numCanciones;
+		int [] canciones;
+		int[][] usoMax;
+		int[][] rastroMigajas;
+		
+		public Converter(String data){
+			String[] numbers = data.split(" ");
+			maximo = Integer.parseInt(numbers[0]);
+			numCanciones = Integer.parseInt(numbers[1]);
+			canciones = new int [numCanciones];
+			usoMax = new int [numCanciones+1][maximo+1]; 
+			rastroMigajas = new int[numCanciones+1][maximo+1];
+			
+			for (int i = 2, j = 0; i < numbers.length; i++, j++) {
+				canciones[j] = Integer.parseInt(numbers[i]);
+			}
+		}
+		
+		public void mostrarSuma(){
+			System.out.println("Sum: " + usoMax[numCanciones][maximo]);
+		}
+		
+		public String getSuma(){
+			return usoMax[numCanciones][maximo] + "";
+		}
+		
+		public int[][] getUsoMax(){
+			return usoMax;
+		}
+		
+		public int[][] getRastroMigajas(){
+			return rastroMigajas;
+		}
+
+		public int getMaximo() {
+			return maximo;
+		}
+
+		public void setMaximo(int maximo) {
+			this.maximo = maximo;
+		}
+
+		public int getNumCanciones() {
+			return numCanciones;
+		}
+
+		public void setNumCanciones(int numCanciones) {
+			this.numCanciones = numCanciones;
+		}
+
+		public int[] getCanciones() {
+			return canciones;
+		}
+
+		public void setCanciones(int[] canciones) {
+			this.canciones = canciones;
+		}
+		
+		public void mostrarMatriz(){
+			for (int i = 1; i < numCanciones+1; i++) {
+				for (int j = 1; j < maximo+1; j++) {
+					System.out.print(usoMax[i][j]);
+					System.out.print("\t");
+				}
+				System.out.println("");
+			}
+		}
+		
+		public void mostrarMatrizMigajas(){
+			for (int i = 1; i < numCanciones+1; i++) {
+				for (int j = 1; j < maximo+1; j++) {
+					System.out.print(rastroMigajas[i][j]);
+					System.out.print("\t");
+				}
+				System.out.println("");
+			}
+		}
+		
+		public void mostrarSolucion(int[] solucion){
+			
+			for (int i = 0; i < solucion.length; i++) {
+					if(solucion[i] != 0){
+						System.out.print(solucion[i]);		
+						System.out.print(" ");
+					}
+			}
+		}
+	}
+	
+}
---
+++ Algo3Final/src/problema2/Main.java
@@ -0,0 +1,230 @@
+package problema2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    	
+    	//City city = new City("7 0 1 0 2 0 4 2 4 2 3 3 1 4 3");
+    	//City city = new City("5 0 2 0 1 1 5 2 5 2 1");
+    	//City city = new City("9 0 1 0 2 0 3 0 4 1 4 2 1 2 0 3 0 3 1");
+    	String datos = Consola.leerDatos();
+    	Parser parser = new Parser(datos);
+    	
+    	
+    	for (City city : parser.getCities()) {
+    		obtenerCaminos(city);
+		}
+
+    }
+    
+    private static void obtenerCaminos(City city){
+    	//Converter converter = new Converter(datos);
+    	
+    	int max = city.getMax();
+    	int matAdj[][] = city.getMatAdj();
+    	int matCaminoI[][] = city.copyMatriz(matAdj);
+    	int matSuma[][] = city.copyMatriz(matAdj);
+    	
+    	for (int i = 1; i < max+1;  i++) {
+    		matCaminoI = city.multiplicarMatrices(matCaminoI,matAdj);
+			matSuma = city.sumarMatrices(matSuma,matCaminoI);
+		}
+    	
+    	System.out.println("matrix for city " + city.getName());
+    	city.marcarCaminosInfinitos(matSuma);
+    	city.mostrarMatriz(matSuma);
+    }
+    
+    private static class Consola{
+    	
+    	public static String leerDatos() {
+    		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    		String datos = "";
+    		String aux;
+    		try{
+    			aux = br.readLine();
+    		
+	    		while (aux!=null && !aux.equals("")){
+	    			datos = datos + aux + " ";
+	    			aux = br.readLine(); 
+	    		}
+    		}
+    		catch(IOException e){
+    			aux = null;
+    		}
+    		return datos;
+    	}
+    }
+
+    private static class Parser{
+    
+    	private List<City> cities = new ArrayList();
+    	
+    	public Parser(String datos){
+    		int EXTRA_SPACE = 1;
+    		int CANT_SPACES = 2;
+    		int CANT_ELEMS = 2;
+    		
+    		String dato;
+    		int ejes;
+    		int index = 0;
+    		int name = 0;
+    		while(index < datos.length()){
+    			ejes = Integer.parseInt(datos.charAt(index) + "");
+    			dato = datos.substring(index, index + (ejes *(CANT_SPACES + CANT_ELEMS) + 1));
+        		cities.add(new City(dato,name));
+    			index = index + dato.length() + EXTRA_SPACE;
+    			name++;
+			}
+    	}
+    	
+    	public List<City> getCities(){
+    		return cities;
+    	}
+    }
+    
+    private static class City{
+    	
+    	int name;
+    	int ejes;
+		int[][] matAdj;
+		int max;
+    	
+		public City(String data){
+			this(data,0);
+		}
+		
+    	public City(String data, int name){
+    		this.name = name;
+			String[] numbers = data.split(" ");
+			ejes = Integer.parseInt(numbers[0]);
+			
+			max = 0;
+			for (int i = 1;  i < numbers.length; i++) {
+				int nodo = Integer.parseInt(numbers[i]);
+				if(nodo > max){
+					max = nodo;
+				}
+			}
+			max = max + 1;
+			
+			matAdj = new int [max][max];
+			
+			for (int i = 1;  i < numbers.length-1; i=i+2) {
+				int f = Integer.parseInt(numbers[i]);
+				int c = Integer.parseInt(numbers[i+1]);
+				matAdj[f][c] = 1;
+			}
+		}
+
+    	public int getName() {
+			return name;
+		}
+    	
+		public int[][] getMatAdj() {
+			return matAdj;
+		}
+
+		public int getMax() {
+			return max;
+		}
+
+		public void mostrarMatriz(){
+			for (int i = 0; i < max; i++) {
+				for (int j = 0; j < max; j++) {
+					System.out.print(matAdj[i][j]);
+					System.out.print("\t");
+				}
+				System.out.println("");
+			}
+		}
+		
+		public void mostrarMatriz(int mat[][]){
+			for (int i = 0; i < max; i++) {
+				for (int j = 0; j < max; j++) {
+					System.out.print(mat[i][j]);
+					System.out.print("\t");
+				}
+				System.out.println("");
+			}
+		}
+		
+		public int[][] multiplicarMatrices(int[][] mat1, int [][] mat2){
+			int suma = 0;
+			int matRes[][] = new int[max][max];
+			
+			for (int f = 0; f < max; f++) {
+				for (int c = 0; c < max; c++) {
+					int i = 0;
+					suma = 0;
+					for (i = 0; i < max; i++) {
+						suma = suma + mat1[f][i] * mat2[i][c]; 
+					}
+					matRes[f][c] = suma;
+				}
+				
+
+			}
+			return matRes;
+		}
+		
+		public int[][] copyMatriz(int mat[][]){
+			
+			int matRes[][] = new int[max][max];
+				
+				for (int i = 0; i < max; i++) {
+					for (int j = 0; j < max; j++) {
+						matRes[i][j] = mat[i][j];
+					}
+				}
+			return matRes;
+		}
+		
+		public int[][] sumarMatrices(int mat1[][], int mat2[][]){
+			
+			int matRes[][] = new int[max][max];
+				
+				for (int i = 0; i < max; i++) {
+					for (int j = 0; j < max; j++) {
+						matRes[i][j] = mat1[i][j] + mat2[i][j];
+					}
+				}
+			return matRes;
+		}
+		
+		@Deprecated
+		public boolean isMatrizCero(int mat[][]){
+			for (int i = 0; i < max; i++) {
+				for (int j = 0; j < max; j++) {
+					if((mat[i][j]!=0)){
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		
+		public void marcarCaminosInfinitos(int mat[][]){
+			for (int i = 0; i < max; i++) {
+				for (int j = 0; j < max; j++) {
+					if((mat[i][j] > max)){
+						mat[i][j] = -1;
+					}
+				}
+			}
+		}
+		
+    }
+    
+    public static void enter(){
+    	System.out.println("");
+    }
+        
+}
