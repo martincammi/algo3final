9c77caaf062d85105d8a2bb2b3843b3a2e366c1f
martincammi <martincammi@6a4e8b92-d0e5-a920-00eb-13bb9fe8d438>
2011-11-06 18:02:40 +0000

    Test para Encontrar MatchingNodos

--- Algo3Tp3/src/tests/TestEncontrarMatchingNodos.java
+++ Algo3Tp3/src/tests/TestEncontrarMatchingNodos.java
@@ -1,23 +1,25 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
-import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import algorithms.CarteroGrasp;
-
 import utils.GeneradorGrafos;
 import utils.Grafo;
+import algorithms.CarteroConstructiva;
 
-public class TestEncontrarParesNodos {
+public class TestEncontrarMatchingNodos {
 
 	@Test
 	public void testGrafoSumidero(){
 		Grafo grafo = GeneradorGrafos.generarGrafoOrSumidero();
-		CarteroGrasp cartero = new CarteroGrasp(grafo);
+		CarteroConstructiva cartero = new CarteroConstructiva(grafo);
 		System.out.println("Grafo de " + grafo.cantNodos);
-		List<int[]> pares = cartero.encontrarParesNodos();
+		List<int[]> pares = cartero.encontrarMatchingNodos();
 		
 		assertNotNull(pares);
 		assertEquals(pares.size(), grafo.cantNodos-2-1);
